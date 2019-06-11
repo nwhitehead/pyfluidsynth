61503b910083e20bef8623dd7aa0549b2df96516
Christopher Arndt <chris@chrisarndt.de>
2019-06-11 13:57:56 +0200

    Reformat setup.py and switch 'future' dependency for 'six'
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""\
+Python bindings for FluidSynth, a MIDI synthesizer using SoundFont instruments.
+
+FluidSynth is a software synthesizer for generating music.  It works like a
+MIDI synthesizer.  You load patches, set parameters, then send NOTEON and
+NOTEOFF events to play notes.  Instruments are defined in SoundFonts,
+generally files with the extension SF2.  FluidSynth can either be used to play
+audio itself, or you can call a function that returns chunks of audio data and
+output the data to the soundcard yourself.
+"""
+
 from setuptools import setup
 
-setup (name = 'pyFluidSynth',
-       version = '1.3',
-       author = 'Nathan Whitehead',
-       author_email = 'nwhitehe@gmail.com',
-       url = 'https://github.com/nwhitehead/pyfluidsynth',
-       description = 'Python bindings for FluidSynth, a MIDI synthesizer that uses SoundFont instruments',
-      long_description = '''
-This module contains python bindings for FluidSynth.  FluidSynth is a
-software synthesizer for generating music.  It works like a MIDI
-synthesizer.  You load patches, set parameters, then send NOTEON and
-NOTEOFF events to play notes.  Instruments are defined in SoundFonts,
-generally files with the extension SF2.  FluidSynth can either be used
-to play audio itself, or you can call a function that returns chunks
-of audio data and output the data to the soundcard yourself.
-''',
-       py_modules = ['fluidsynth'],
-       install_requires = ['future'])
+setup(
+    name='pyFluidSynth',
+    version='1.3',
+    author='Nathan Whitehead',
+    author_email='nwhitehe@gmail.com',
+    url='https://github.com/nwhitehead/pyfluidsynth',
+    description=__doc__.splitlines()[0],
+    long_description="\n".join(__doc__.splitlines()[2:]),
+    py_modules=['fluidsynth'],
+    install_requires =['six'],
+    zip_safe=True
+)
