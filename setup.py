98b22fb8e863502684f390c57749b487b1e73e77
chris <romberg@ffmfo.de>
2024-02-18 22:04:33 +0100

    Bump version and changelog

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup (name = 'pyFluidSynth',
-       version = '1.3.2',
+       version = '1.3.3',
        author = 'Nathan Whitehead',
        author_email = 'nwhitehe@gmail.com',
        url = 'https://github.com/nwhitehead/pyfluidsynth',
